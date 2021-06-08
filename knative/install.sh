a8d1e3f6416f5f48801bf580dd1817e06db007a1
mohammad katoozi <k2527806@gmail.com>
2021-06-08 20:14:48 +0430

    add some usefull commands for knative

--- knative/install.sh
+++ knative/install.sh
@@ -5,3 +5,12 @@ cd client
 ./hack/build.sh -f
 
 sudo mv kn /usr/bin/kn
+
+sudo chown root:wheel /home/$USER/.minikube/bin/docker-machine-driver-hyperkit
+
+minikube start --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+
+curl -sL https://run.solo.io/gloo/install | sh
+export PATH=$HOME/.gloo/bin:$PATH
+
+glooctl install knative -g --install-knative-version="0.15.0"
